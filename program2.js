e79de421ee5cd44e2c6381da81815429ed48ea28
Gangadhar Marka <markagangadhar7@gmail.com>
2024-11-11 13:35:24 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,7 +1,11 @@
 const decodeTheRing = function (s, p) {
-
-    // write your code here
-
+  const match = (i, j) => {
+      if (j === p.length) return i === s.length;
+      if (p[j] === '*') return match(i, j + 1) || (i < s.length && match(i + 1, j));
+      if (p[j] === '?' || p[j] === s[i]) return i < s.length && match(i + 1, j + 1);
+      return false;
   };
-  
-  module.exports = decodeTheRing;
+  return match(0, 0);
+};
+
+module.exports = decodeTheRing;
