b9fb5759785ccc7d0bb3f5a79504b3b0f87d064f
Gangadhar Marka <markagangadhar7@gmail.com>
2024-11-11 13:39:58 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,21 +1,16 @@
 const getTotalIsles = function (grid) {
   if (!grid || grid.length === 0) return 0;
-
   let islandCount = 0;
-
   const dfs = (i, j) => {
       if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] === 'W') {
           return;
       }
-
       grid[i][j] = 'W';
-
       dfs(i + 1, j);
       dfs(i - 1, j);
       dfs(i, j + 1);
       dfs(i, j - 1);
   };
-
   for (let i = 0; i < grid.length; i++) {
       for (let j = 0; j < grid[0].length; j++) {
           if (grid[i][j] === 'L') {
@@ -24,8 +19,6 @@ const getTotalIsles = function (grid) {
           }
       }
   }
-
   return islandCount;
 };
-
 module.exports = getTotalIsles;
